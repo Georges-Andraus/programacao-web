cf2ff991b91fbf66143db3289f307240fa5ef490
Georges Andraus <Georges.andraus@sempreceub.com>
2024-08-20 10:10:36 -0300

    Alterado o css do login, mudança de fundo e validação de cpf

--- script.js
+++ script.js
@@ -27,15 +27,67 @@ function validateNome(nome) {
 }
 function validateCPF(cpf) {
     cpf = cpf.trim();
+    let cpfparttern2 = /^\d{11}$/
+    let cpfparttern = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
     if (cpf == "" ) {
         alert("O CPF nao pode estar vazio");
         return false;
     }
-    let cpfparttern = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
-    if(!cpfparttern.test(cpf)){
-        alert("O CPF deve siguir esse formato: 000.000.000-00");
+    if(!cpfparttern.test(cpf) && !cpfparttern2.test(cpf)){
+        alert("O CPF deve siguir esse formato: 000.000.000-00 ou 00000000000");
         return false;
     }
+    cpf = cpf.replace(/\D/g, '');
+    if(!validateDV1(cpf) || !validateDV2(cpf)){
+        alert("CPF invalido");
+        return false;
+    }
+    return true;
+}
+
+function validateDV1(cpf) {
+    let soma = 0;
+    for(let i=1; i<=9; i++){
+        soma += (cpf.charAt(i - 1) * (10 - (i - 1)));
+    }
+    console.log(soma);
+    let resto = soma % 11;
+
+    if(resto < 2){
+        if(cpf.charAt(9) != 0){
+            return false;
+        }
+    }
+
+    let digitoVerificador = 11 - resto;
+    if(digitoVerificador != cpf.charAt(9)){
+        return false;
+    }
+
+
+    return true;
+}
+function validateDV2(cpf) {
+    let soma = 0;
+    for(let i=1; i<=10; i++){
+        soma += (cpf.charAt(i - 1) * (11 - (i - 1)));
+    }
+    console.log(soma);
+    let resto = soma % 11;
+
+    if(resto < 2){
+        if(cpf.charAt(10) != 0){
+            return false;
+        }
+    }
+
+    let digitoVerificador = 11 - resto;
+    if(digitoVerificador != cpf.charAt(10)){
+        return false;
+    }
+
+
+    return true;
 }
 
 function validateSenha(senha) {
